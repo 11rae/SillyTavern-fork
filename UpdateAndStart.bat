f29825128b054416e2526bb27a309b5b04344339
11rae <11rae@proton.me>
2026-07-04 12:13:56 +0000

    Fix batch scripts

--- UpdateAndStart.bat
+++ UpdateAndStart.bat
@@ -20,8 +20,8 @@ if %errorlevel% neq 0 (
     )
 )
 set NODE_ENV=production
-call npm install --no-save --no-audit --no-fund --loglevel=error --no-progress --omit=dev --ignore-scripts
-node server.js %*
+call deno install --prod -q
+deno task start:deno %*
 :end
 pause
 popd
