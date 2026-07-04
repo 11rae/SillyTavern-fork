6fc66ddc76cd0e34cb1ddfb4c13e7045fb7c16ef
11rae <11rae@proton.me>
2026-07-04 12:13:57 +0000

    Set NODE_ENV to production in start.sh

--- start.sh
+++ start.sh
@@ -14,4 +14,4 @@ deno install --prod -q
 
 echo "Entering SillyTavern..."
 # Since Deno refuses to work when LD_PRELOAD is set and Termux sets LD_PRELOAD for compatibility reasons, we unset it temporarily.
-(unset LD_PRELOAD && deno task start:deno $@)
+(unset LD_PRELOAD && export NODE_ENV=production && deno task start:deno $@)
